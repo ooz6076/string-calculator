c1a9b5ac3037590005d627fe73935462aa0f206d
Akhilesh <b170124@nitsikkim.ac.in>
2020-10-24 15:17:10 +0530

    Refactor: Redundancy reduced

--- src/main/java/TestUnit.java
+++ src/main/java/TestUnit.java
@@ -13,22 +13,30 @@ public class TestUnit {
     public void testOne(){
         // test for null String
         int res = stringCalculator.add("0");
-        assert res == 0;
-        System.out.println("Null String test = " + res);
-        System.out.println("Test passed");
+        assertEquals("Null String test",0,res);
     }
 
     @Test
     public void testTwo(){
         //test for empty String
         int res = stringCalculator.add("");
-        assert res == 0;
-        System.out.println("Empty String test = " + res);
-        System.out.println("Test passed");
+        assertEquals("Empty String test = ",0,res);
     }
 
     @After
     public void destroy(){
         stringCalculator = null;
+    }
+
+    private void assertEquals(String testCase, int actual, int result){
+        // prints result for each test case
+        System.out.println(testCase + " = " + result);
+
+        // checks if test case passed
+        if(actual == result){
+            System.out.println("Test passed");
+        }else{
+            System.out.println("Test failed!");
+        }
     }
 }
