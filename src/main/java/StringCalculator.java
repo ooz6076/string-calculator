b72e9b97036d8440e36d62b554b26906c808852e
Akhilesh <b170124@nitsikkim.ac.in>
2020-10-24 15:11:30 +0530

    Empyt and null string test done

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,4 +5,23 @@ public class StringCalculator {
 
     }
 
+    public int add(String numbers){
+
+
+        if(numbers == null || numbers.isEmpty()){
+            return 0;
+        }
+
+        String[] nums = numbers.split(",", 0);
+
+        int sum = 0;
+
+        for(String number: nums){
+            int num = Integer.parseInt(number);
+            sum += num;
+        }
+
+        return sum;
+    }
+
 }
--- src/main/java/TestUnit.java
+++ src/main/java/TestUnit.java
@@ -11,7 +11,20 @@ public class TestUnit {
 
     @Test
     public void testOne(){
-        
+        // test for null String
+        int res = stringCalculator.add("0");
+        assert res == 0;
+        System.out.println("Null String test = " + res);
+        System.out.println("Test passed");
+    }
+
+    @Test
+    public void testTwo(){
+        //test for empty String
+        int res = stringCalculator.add("");
+        assert res == 0;
+        System.out.println("Empty String test = " + res);
+        System.out.println("Test passed");
     }
 
     @After
